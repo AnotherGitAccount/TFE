8184507f72f0827ed1f77436dcf6b2986aab7cbe
Quentin <poletquentin0@gmail.com>
2021-04-23 22:03:14 +0200

    Adding asm files

--- asm/checkerboard.asm
+++ asm/checkerboard.asm
@@ -3,14 +3,14 @@
 | Constants
 HEIGHT = 54
 WIDTH  = 72
-BLUE   = 0b000000001111
-YELLOW = 0b111111110000
-RED    = 0b111100000000
-GREEN  = 0b000011110000
+BLUE   = 0b0000000000001111
+YELLOW = 0b0000111111110000
+RED    = 0b0000111100000000
+GREEN  = 0b0000000011110000
 
 | Clears all used registers
 init:
-    zero   = R31
+    zero   = R0
     posy   = R1
     posx   = R2
     vaddr  = R3
@@ -28,15 +28,14 @@ loop_x:
     SUBC(posx, 1, posx)
 draw:
     CMOVE(0x0010, vaddr)    | adds 1 at the beginning of the word (graphical mode)
-    SHLC(vaddr, 16, vaddr)  | shifts posx 
+    SHLC(vaddr, 15, vaddr)  | shifts posx 
     OR(vaddr, posx, vaddr)  | adds posx 
     SHLC(vaddr, 6, vaddr)   | shifts posx 
     OR(vaddr, posy, vaddr)  | adds posy
-    SHLC(vaddr, 5, vaddr)   | puts block location in [18:6]
-
+    SHLC(vaddr, 6, vaddr)   | puts block location in [18:6]
     XOR(posx, posy, tmp1)    | xor to decide which tile it is
-    ORC(zero, 0x0001, tmp2) | creates the mask
-    AND(tmp1, tmp2, tmp1)   | apply mask
+    ORC(zero, 0x0001, tmp2)  | creates the mask
+    AND(tmp1, tmp2, tmp1)    | apply mask
     BNE(tmp1, tile2)
 tile1:
     CMOVE(BLUE, vdata)         | adds blue color
@@ -46,9 +45,9 @@ tile1:
 tile2:
     CMOVE(RED, vdata)         | adds red color
     SHLC(vdata, 12, vdata)    | shift red
-    ADDC(vdata, GREEN, vdata) | adds green color
+    ORC(vdata, GREEN, vdata)  | adds green color
 end_draw:
     ST(vdata, 0, vaddr)       | draw
-    | END_CODE
+    | END_CODE 
     BNE(posx, loop_x)
     BNE(posy, loop_y)
